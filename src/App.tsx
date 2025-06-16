a4f8eb6244e53cb392d9742e772f9bef5b795e1c
aazis7 <azis190407@gmail.com>
2025-06-16 12:48:26 +0000

    feat: error boundary

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
 import { Route, Switch } from "wouter";
+import { ErrorBoundary } from "@/components/error-boundary.tsx";
+
+function MyError() {
+  const isErr = false;
+  if (!isErr) throw new Error("Test");
+
+  return <div>MyError</div>;
+}
 
 export default function App() {
   return (
-    <Switch>
-      <Route path="/">
-        <div>Home</div>
-      </Route>
-      <Route>
-        <div>404</div>
-      </Route>
-    </Switch>
+    <ErrorBoundary>
+      <Switch>
+        <Route path="/">
+          <div>Home</div>
+        </Route>
+        <Route path="/error">
+          <MyError />
+        </Route>
+        <Route>
+          <div>404</div>
+        </Route>
+      </Switch>
+    </ErrorBoundary>
   );
 }
